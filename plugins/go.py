6c5682b1177610c0d205bb980b2e63f71f2402ad
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-03-29 10:42:14 +0530

    Update go.py

--- plugins/go.py
+++ plugins/go.py
@@ -1,3 +1,4 @@
+
 from telethon import events, TelegramClient
 from asyncio import sleep as zzz
 from random import randint
@@ -10,7 +11,7 @@ chat = 572621020
 hunt = False
 list = ["A wild"]
 
-@kanha_bot.on(events.NewMessage(outgoing=True, pattern='.go'))
+@kanha_bot.on(events.NewMessage(outgoing=True, pattern='.bgo'))
 async def begin(event):
     global hunt
     hunt = True
@@ -19,7 +20,7 @@ async def begin(event):
     try:
         async with kanha_bot.conversation('@Hexamonbot') as conv:
             await conv.get_response(x.id)
-    except:
+    except Exception as e:
         await zzz(1, 3)
         await kanha_bot.send_message(chat, "/hunt")
 
@@ -30,20 +31,20 @@ async def handle_hunt(event):
         text = event.message.text
         hun = True
         message = await kanha_bot.get_messages(chat, ids=event.message.id)
-        if "A shiny" in text and hunt:
+        if "✨" in event.raw_text:
+            await zzz(randint(1, 2))
+            await message.click(text='Battle')
             await message.click(text='Poke Balls')
-            await zzz(randint(5, 7))
             await message.click(text='Ultra')
             await message.click(text='Great')
-            pass
-        elif "TM" in text and hunt:
+        elif "TM" in event.raw_text:
             print(event.message.text)
             await zzz(randint(5, 7))
             x = await kanha_bot.send_message(chat, "/hunt")
             try:
                 async with kanha_bot.conversation('@Hexamonbot') as conv:
                     await conv.get_response(x.id)
-            except:
+            except Exception as e:
                 await zzz(3, 7)
                 await kanha_bot.send_message(chat, "/hunt")
         elif any(item in text for item in list) and hunt:
@@ -59,40 +60,34 @@ async def handle_hunt(event):
                 try:
                     async with kanha_bot.conversation('@Hexamonbot') as conv:
                         await conv.get_response(x.id)
-                except:
+                except Exception as e:
                     await zzz(3, 5)
                     await kanha_bot.send_message(chat, "/hunt")
-
-@kanha_bot.on(events.NewMessage(outgoing=True, pattern='.bstop'))
-async def stop(event):
-    await event.edit('stopped hexa battling')
-    global hunt
-    hunt = False
 
 @kanha_bot.on(events.MessageEdited(chats=chat))
 async def cacther(event):
     if hunt:
         message = await kanha_bot.get_messages(chat, ids=event.message.id)
+        await zzz(2)
         await message.click(1, 1)
+        await message.click(0, 0)
+        await message.click(1, 0)
         
-        if any(keyword in event.message.text for keyword in ['fled', 'fainted', 'caught', '+']):
+        if any(keyword in event.message.text for keyword in ['fled', 'caught', '+']):
             await zzz(randint(2, 5))
             x = await kanha_bot.send_message(chat, "/hunt")
             try:
                 async with kanha_bot.conversation('@Hexamonbot') as conv:
                     await conv.get_response(x.id)
-            except:
+            except Exception as e:
                 await zzz(1, 3)
                 await kanha_bot.send_message(chat, "/hunt")
-async def stomp(event):
-    global hunt
-    if hunt:
-        message = await kanha_bot.get_messages(chat, ids=event.message.id)
-        if "Daily limit for battling" in event.message.text and hunt:
-             await zzz(3)
-             await stop(event)
-             hunt = False
 
+@kanha_bot.on(events.NewMessage(outgoing=True, pattern='.bstop'))
+async def stop(event):
+    global hunt
+    hunt = False
+    await event.edit('stopping hexa battles')
 
 @kanha_bot.on(events.NewMessage(chats=chat, incoming=True))
 async def handle_battle(event):
@@ -102,3 +97,5 @@ async def handle_battle(event):
             message = await kanha_bot.get_messages(chat, ids=event.message.id)
             await zzz(2)
             await message.click(0, 1)
+            await message.click(0, 0)
+            await message.click(1, 0)
